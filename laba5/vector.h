26cad4628527908c88638e50bbf4cf7bba44645a
barvenov <barvenovsa@gmail.com>
2015-05-18 15:15:52 +0300

    Update vector.h

--- laba5/vector.h
+++ laba5/vector.h
@@ -6,10 +6,16 @@ class Vector
 	private:
 		double x, y;
 	public:
-		Vector() :x(0), y(0){} 
-		Vector(double a, double b) : x(a), y(b){}
+	//TODO Great 4
+		Vector(double a, double b) : x(a), y(b){}	// constructor
+		Vector() :x(0), y(0){}                         // Default constructor
+		// destructor
+		// copy constructor
+		// operator=
+		
 		void enterVector(double, double);
-		void vector_print();
+		void vector_print();//TODO лучше перегрузить оператор <<
+		//TODO нет операторов SET и GET
 		void normalize();
 		double dot_product(Vector);
 		Vector multiple(double);
