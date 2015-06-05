36990575af899d65387064319f8847e8f2b86b4f
innasuslenok <innasuslenok@gmail.com>
2015-06-05 09:23:26 +0300

    Update main.cpp

--- laba5/main.cpp
+++ laba5/main.cpp
@@ -8,17 +8,12 @@ int main()
     Vector v={2,3}, a={3,4},rez;
 
     v.vector_print();
-    // TODO чтобы заработало cout<<"first vector= "<<v<<" Fnd now, second:"endl;
     cout<<endl;
 
 	v.multiple(0.5).vector_print();
         cout<<endl;
 	cout<<v.dot_product(a)<<endl;
     cout<<endl;
-/* TODO
-	rez=v*5;cout<<rez<<endl;
-	rez=v*a;cout<<rez<<endl;
-*/
     v.normalize();
     v.vector_print();
 
